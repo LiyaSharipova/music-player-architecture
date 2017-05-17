7652944e319f1c54f984378d97b76d09606dd9fe
liyasharipova <liyasharipova2107@gmail.com>
2017-05-17 17:25:29 +0300

    readme

--- src/controller/ViewController.php
+++ src/controller/ViewController.php
@@ -6,11 +6,17 @@
  * Получает данные при нажатии на кнопки
  * Отправляет данные для отображения на GUI
  */
-interface ViewCommunicator
+interface ViewController
 {
     /**
      * Отправка данных на View
      * @param array $datatoViewDtos массив данных, для отправки на View
      */
     public function sendDataToView(array $datatoViewDtos);
+
+    /**
+     * Отправляет начальные данные на View->printDataToConsole(), для дальнейшего вывода на консоль и выбора трека
+     * @return mixed
+     */
+    public function sendInitialInfo();
 }
